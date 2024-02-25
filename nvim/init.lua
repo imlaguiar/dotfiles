342a9fc805710495cf5a8427a28701114b6c19b8
imlaguiar <leo_aguiar10@hotmail.com>
2024-02-24 21:57:18 -0300

    Added a few remaps and treesitter context

--- nvim/init.lua
+++ nvim/init.lua
@@ -206,19 +206,11 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+  }
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+
 }, {})
 
 -- [[ Setting options ]]
@@ -625,8 +617,14 @@ vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
+-- After jumping center around cursor pleaseee
 vim.keymap.set("n", "''", "''zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "<S-g>", "<S-g>zz")
+vim.keymap.set("n", "gg", "ggzz")
+
+-- Highlight the cursor line
+vim.opt.cursorline = true
